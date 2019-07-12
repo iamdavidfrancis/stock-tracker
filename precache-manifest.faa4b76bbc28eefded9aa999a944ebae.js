7eb9f3d9d87032d9e186e6ee500443006bce4eb3
David Francis <iamdavidfrancis@gmail.com>
2019-07-12 14:56:56 -0700

    Updates

---
+++ precache-manifest.faa4b76bbc28eefded9aa999a944ebae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bf754cb5d1e7cbeb0014befe5645ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdce1b47b5a6f4e35172",
+    "url": "/static/css/2.d4feb5be.chunk.css"
+  },
+  {
+    "revision": "12d6b8dbe0e8d49a3679",
+    "url": "/static/css/main.e6e7cd13.chunk.css"
+  },
+  {
+    "revision": "bdce1b47b5a6f4e35172",
+    "url": "/static/js/2.6053b1c5.chunk.js"
+  },
+  {
+    "revision": "12d6b8dbe0e8d49a3679",
+    "url": "/static/js/main.caeedce9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
